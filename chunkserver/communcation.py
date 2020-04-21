2bd3382227e6624753799e519dd679d2fd9223d7
Shivangi <sivangi.singh@students.iiit.ac.in>
2020-04-22 02:29:18 +0530

    chunkserver added

--- chunkserver/communcation.py
+++ chunkserver/communcation.py
@@ -8,7 +8,7 @@ DUPLICATE_MASTER_IP = "127.0.0.1"
 DUPLICATE_MASTER_PORT = 8081
 MAX_CHUNK_SIZE = 2048
 
-class masterthread():
+class chunkserver():
 	dirty = False
 	dirty_chunks = []
 	mutual_excl = {}
@@ -41,7 +41,9 @@ class masterthread():
 		sock.bind(("127.0.0.1",self.myport))
 		sock.listen(5)
 		while True:
+			
 			client, address = sock.accept()
+			client.settimeout(60)
 			threading.Thread(target = self.checkoperation,args = (client,address)).start()
 
 	def checkoperation(self,client,address):
@@ -52,21 +54,22 @@ class masterthread():
 			if(to_recv[1]=="heartbeat"):
 				self.heartbeat_reply(client)
 			elif(to_recv[1]=="copy"):
-				self.copyfromchunkserver(client,recv[12:])
+				client.close()
+				self.copyfromchunkserver(recv[12:])
 		elif(to_recv[0]=="client"):
 			if to_recv[1]=="read":
 				self.sendchunk(to_recv,client,address)
 			elif to_recv[1]=="append":
-				if len(mutual_excl[to_recv[2]])!=0:
+				if len(self.mutual_excl[to_recv[2]])!=0:
 					mutual = [to_recv,client]
-					mutual_excl[recv[2]].append(mutual)
+					self.mutual_excl[recv[2]].append(mutual)
 				else:
 					self.appendchunk(to_recv,client)
 		elif(to_recv[0]=="chunkserver"):
 			if(to_recv[1]=="appendinfo"):
-				if len(mutual_excl[to_recv[2]])!=0:
+				if to_recv[2] in (self.mutual_excl).keys():
 					mutual = [to_recv,client]
-					mutual_excl[recv[2]].append(mutual)
+					((self.mutual_excl)[to_recv[2]]).append(mutual)
 				else:
 					self.appendchunk(to_recv,client)
 			elif(to_recv[1]=="sendcopy"):
@@ -76,37 +79,37 @@ class masterthread():
 	def heartbeat_reply(self,client):
 		print("reply to heartbeat msg")
 		if(self.dirty == False):
-			client.send(bytes([0]))
-			client.close()
+			client.sendall(bytes([0]))
 		else:
-			num = sys.getsizeof(self.dirty_chunks)
-			client.send(bytes[num])
+			nums = str(sys.getsizeof(self.dirty_chunks))
+			# print(type(nums))
+			client.sendall(nums.encode())
 			client.recv(60)
 			client.sendall((str(self.dirty_chunks)).encode())
-			client.close()
 			self.dirty = False
 			self.dirty_chunks = []
-
-	def copyfromchunkserver(self,client,copylist):
 		client.close()
-		print("copy from chunk server given")
+
+	def copyfromchunkserver(self,copylist):
+		# client.close()
+		print("copy from chunkserver")
 		copylist = copylist[1:-1].split(',')
 		for item in copylist:
 			item = item[1:-1].split('=')
 			chunkname = item[1]
+			self.dirty = True
+			self.dirty_chunks.append(chunkname)
 			item = item[0].split(":")
 			serverip, serverport  = item[0],item[1]
 			tosend = "chunkserver:sendcopy:"+chunkname
 			s1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			s1.connect((serverip, int(serverport)))
-			s1.send(tosend.encode())
+			s1.sendall(tosend.encode())
 			chunk = self.path+"/"+chunkname
-			with open(chunk,'ab') as f1:
-				data = client.recv(MAX_CHUNK_SIZE)
+			with open(chunk,'wb') as f1:
+				data = s1.recv(MAX_CHUNK_SIZE)
 				f1.write(data)
 			s1.close()
-			self.dirty = True
-			self.dirty_chunks.append(chunkname)
 			print("chunk recieved")
 
 	def sendchunk(self,to_recv,client,address):
@@ -114,53 +117,61 @@ class masterthread():
 		chunk=self.path+"/"+to_recv[2]
 		with open(chunk, 'rb') as f:
 			data=f.read(MAX_CHUNK_SIZE)
-			client.send(data)
-		client.close()
+			client.sendall(data)
 		print("chunk sent")
+		client.close()
 
 	def appendchunk(self,recv,client_con):
-		print("append")
+		print("appending data")
+		(self.mutual_excl)[recv[2]] = []
 		mutual = [recv,client_con]
-		mutual_excl[recv[2]].append(mutual)
-		while len(mutual_excl[recv[2]])!=0:
+		(self.mutual_excl)[recv[2]].append(mutual)
+		while len((self.mutual_excl)[recv[2]])!=0:
+			conn =  (self.mutual_excl)[recv[2]][0]
 			to_recv,client = conn[0],conn[1]
 			chunk=self.path+"/"+to_recv[2]
 			sizetoappend=int(to_recv[3])
 			file_stats = os.stat(chunk)
 			currsize = file_stats.st_size
 			recvsize = MAX_CHUNK_SIZE - currsize
-			client.send("ok".encode())
+			ind = chunk.rfind('/')
+			file = chunk[ind+1:]
+			filechunk = file.split('_')
+			file = filechunk[0]
+			chunknum = int(filechunk[1])
+			client.sendall("ok".encode())
+			print("curr size ", currsize)
+			print("recv size", recvsize)
 			if recvsize >= sizetoappend:
+				self.dirty = True	
+				self.dirty_chunks.append(to_recv[2])
 				with open(chunk,'ab') as f1:
 					data = client.recv(sizetoappend)
 					f1.write(data)
-				self.dirty = True	
-				self.dirty_chunks.append(to_recv[2])
+				
 			else:
-				ind = chunk.rfind('/')
-				file = chunk[ind+1:]
-				filechunk = file.split('_')
-				file = filechunk[0]
-				chunknum = int(filechunk[1])
-
-				with open(chunk,'ab') as f1:
-					f1.write(client.recv(recvsize))
+				
 				self.dirty = True	
 				self.dirty_chunks.append(to_recv[2])
 
-				chunk = self.path+"/"+file+"_"+str(chunknum+1)
-				with open(chunk, 'ab') as f1:
+				chunk2 = self.path+"/"+file+"_"+str(chunknum+1)
+
+				self.dirty_chunks.append(file+"_"+str(chunknum+1))
+				with open(chunk,'ab') as f1:
+					data = client.recv(recvsize)
+					f1.write(data)
+				
+				with open(chunk2, 'ab') as f1:
 					data2=client.recv(sizetoappend-recvsize)
 					f1.write(data2)
 					data+=data2	
+				
 
-				self.dirty = True	
-				self.dirty_chunks.append(file+"_"+str(chunknum+1))
-			client.close()
 			if to_recv[0]=="client":
 				self.sendtosecondary(data,sizetoappend,to_recv[2])
 
-			mutual_excl[recv[2]].pop(0)
+			self.mutual_excl[recv[2]].pop(0)
+		print("data appended")
 
 	def sendtosecondary(self,data,sizetoappend,file):
 		try:
@@ -172,7 +183,7 @@ class masterthread():
 				s1.connect((DUPLICATE_MASTER_IP, DUPLICATE_MASTER_PORT))
 			except:      
 				sys.exit()
-		s1.send(("info:"+file).encode())
+		s1.sendall(("info:"+file).encode())
 		getlist = s1.recv(MAX_CHUNK_SIZE).decode()
 		getlist = getlist[1:-1].split(',')
 		s1.close()
@@ -181,10 +192,10 @@ class masterthread():
 			s1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			tosend = "chunkserver:appendinfo:"+file+":"+str(sizetoappend)
 			s1.connect((serverip,serverport))
-			s1.send(tosend.encode())
+			s1.sendall(tosend.encode())
 			st = s1.recv(1024)
-			s1.send(data.encode())
+			s1.sendall(data.encode())
 			s1.close()
 
-master = masterthread()	
+master = chunkserver()	
 master.run()
